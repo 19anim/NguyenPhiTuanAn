8cf8e861b0fb187bdba33d972ca99b05eb06f602
19.anim <nptatdt@gmail.com>
2025-02-06 03:29:39 +0700

    modify: junk code and files in problem2

--- problem2/src/main.js
+++ problem2/src/main.js
@@ -93,20 +93,3 @@ function setTransactionLog() {
     transactionHistory.appendChild(opt);
   }
 }
-
-// document.querySelector("#app").innerHTML = `
-//   <section>
-//     <form onsubmit="return !1">
-//     <h5>Swap</h5>
-//     <label for="input-amount">Amount to send</label>
-//     <input id="input-amount" />
-
-//     <label for="output-amount">Amount to receive</label>
-//     <input id="output-amount" />
-
-//     <button>CONFIRM SWAP</button>
-//   </form>
-//   </section>
-// `;
-
-// setupCounter(document.querySelector("#counter"));
